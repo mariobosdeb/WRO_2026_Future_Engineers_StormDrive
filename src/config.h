a460da711cecbac5047e79d90daff18fe6fe798b
mariobosdeb <barladianu.mariogabriel@gmail.com>
2026-06-06 17:51:22 +0300

    config.h

---
+++ src/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// --- Buton Pornire (Obligatoriu Regulament WRO) ---
+#define START_BUTTON_PIN 2  
+
+// --- Pinii tăi reali pentru Driverul de Motor ---
+#define IN1_PIN 7
+#define IN2_PIN 8
+#define ENA_PIN 9
+
+// --- Pinul tău pentru Servo ---
+#define SERVO_PIN 10
+
+// --- Valorile tale de calibrare ---
+const int DREPT  = 90;
+const int STANGA = 55;
+const int VITEZA = 255;
+
+#endif
